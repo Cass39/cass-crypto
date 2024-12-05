88e301bbcd166547a3a4ff5ef0bac3683fb41827
Cassandra Tolbert <maecass20@gmail.com>
2024-12-04 20:51:17 -0800

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>&copy; 2024 CassCrypto - All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
